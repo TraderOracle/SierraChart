a2339f7bb3489366069dfa6491e09334cdc39014
Trader Oracle <157963314+TraderOracle@users.noreply.github.com>
2024-03-14 08:29:47 -0500

    Add files via upload

--- TraderOracle.cpp
+++ TraderOracle.cpp
@@ -108,60 +108,74 @@ SCSFExport scsf_LindaMACD(SCStudyInterfaceRef sc)
 
 SCSFExport scsf_WaddahExplosion(SCStudyInterfaceRef sc)
 {
+	SCSubgraphRef Subgraph_WaddahPos = sc.Subgraph[0];
+	SCSubgraphRef Subgraph_WaddahNeg = sc.Subgraph[1];
+	SCSubgraphRef Subgraph_Explosion = sc.Subgraph[2];	
+	SCSubgraphRef Subgraph_Slow = sc.Subgraph[3];
+	SCSubgraphRef Subgraph_Fast = sc.Subgraph[4];
+	SCSubgraphRef Subgraph_BB = sc.Subgraph[5];
+
+
     if (sc.SetDefaults)
     {
         sc.GraphName = "Waddah Explosion";
 
-        sc.Subgraph[0].Name = "Ignore";
-		sc.Subgraph[0].DrawStyle = DRAWSTYLE_IGNORE;
+        Subgraph_BB.Name = "BB";
+		Subgraph_BB.DrawStyle = DRAWSTYLE_IGNORE;
 
-        sc.Subgraph[1].Name = "Ignore";
-		sc.Subgraph[1].DrawStyle = DRAWSTYLE_IGNORE;
+        Subgraph_Slow.Name = "Slow";
+		Subgraph_Slow.DrawStyle = DRAWSTYLE_IGNORE;
 
-        sc.Subgraph[5].Name = "Ignore";
-		sc.Subgraph[5].DrawStyle = DRAWSTYLE_IGNORE;
+        Subgraph_Fast.Name = "Fast";
+		Subgraph_Fast.DrawStyle = DRAWSTYLE_IGNORE;
 		
-        sc.Subgraph[2].Name = "Waddah Positive";
-        sc.Subgraph[2].DrawStyle = DRAWSTYLE_BAR_BOTTOM;
-		sc.Subgraph[2].LineWidth = 12;
-		sc.Subgraph[2].PrimaryColor = COLOR_GREEN;
+        Subgraph_WaddahPos.Name = "Waddah Positive";
+        Subgraph_WaddahPos.DrawStyle = DRAWSTYLE_BAR_BOTTOM;
+		Subgraph_WaddahPos.LineWidth = 12;
+		Subgraph_WaddahPos.PrimaryColor = COLOR_GREEN;
 
-        sc.Subgraph[3].Name = "Waddah Negative";
-        sc.Subgraph[3].DrawStyle = DRAWSTYLE_BAR_BOTTOM;
-		sc.Subgraph[3].LineWidth = 12;
-		sc.Subgraph[3].PrimaryColor = COLOR_RED;
+        Subgraph_WaddahNeg.Name = "Waddah Negative";
+        Subgraph_WaddahNeg.DrawStyle = DRAWSTYLE_BAR_BOTTOM;
+		Subgraph_WaddahNeg.LineWidth = 12;
+		Subgraph_WaddahNeg.PrimaryColor = COLOR_RED;
 
-        sc.Subgraph[4].Name = "Explosion Line";
-        sc.Subgraph[4].DrawStyle = DRAWSTYLE_LINE;
-		sc.Subgraph[4].LineWidth = 2;
-		sc.Subgraph[4].PrimaryColor = COLOR_WHITE;
-		sc.Subgraph[4].SecondaryColor = COLOR_RED;
+        Subgraph_Explosion.Name = "Explosion Line";
+        Subgraph_Explosion.DrawStyle = DRAWSTYLE_LINE;
+		Subgraph_Explosion.LineWidth = 2;
+		Subgraph_Explosion.PrimaryColor = COLOR_WHITE;
+		Subgraph_Explosion.SecondaryColor = COLOR_RED;
 
         sc.AutoLoop = 1;
 
         return;
     }
 
-	sc.BollingerBands(sc.BaseData[SC_LAST], sc.Subgraph[5], 20, 2, MOVAVGTYPE_SIMPLE);
-	float UpperBand = sc.Subgraph[5].Arrays[0][sc.Index];
-	float LowerBand = sc.Subgraph[5].Arrays[1][sc.Index];
+	sc.BollingerBands(sc.BaseData[SC_LAST], Subgraph_BB, 20, 2, MOVAVGTYPE_SIMPLE);
+	float UpperBand = Subgraph_BB.Arrays[0][sc.Index];
+	float LowerBand = Subgraph_BB.Arrays[1][sc.Index];
 	float e1 = UpperBand - LowerBand;
-	sc.Subgraph[4][sc.Index] = e1;
+	Subgraph_Explosion[sc.Index] = e1;
 
-	sc.MovingAverage(sc.BaseDataIn[SC_LAST], sc.Subgraph[0], MOVAVGTYPE_EXPONENTIAL, 20);
-	float a1 = sc.Subgraph[0][sc.Index];
-	float b1 = sc.Subgraph[0][sc.Index - 1];
+	sc.MovingAverage(sc.BaseDataIn[SC_LAST], Subgraph_Fast, MOVAVGTYPE_EXPONENTIAL, 20);
+	float a1 = Subgraph_Fast[sc.Index];
+	float b1 = Subgraph_Fast[sc.Index - 1];
 	
-	sc.MovingAverage(sc.BaseDataIn[SC_LAST], sc.Subgraph[0], MOVAVGTYPE_EXPONENTIAL, 40);
-	float a2 = sc.Subgraph[1][sc.Index];
-	float b2 = sc.Subgraph[1][sc.Index - 1];
+	sc.MovingAverage(sc.BaseDataIn[SC_LAST], Subgraph_Slow, MOVAVGTYPE_EXPONENTIAL, 40);
+	float a2 = Subgraph_Slow[sc.Index];
+	float b2 = Subgraph_Slow[sc.Index - 1];
 
 	float t1 = ((a1 - a2) - (b1 - b2)) * 150;
 	
 	if (t1 > 0)
-		sc.Subgraph[2][sc.Index] = t1;
+	{
+		Subgraph_WaddahPos[sc.Index] = t1;
+		Subgraph_WaddahNeg[sc.Index] = 0;
+	}
 	else
-		sc.Subgraph[3][sc.Index] = t1 * -1;
+	{
+		Subgraph_WaddahNeg[sc.Index] = t1 * -1;
+		Subgraph_WaddahPos[sc.Index] = 0;
+	}
 	
     return;
 }
